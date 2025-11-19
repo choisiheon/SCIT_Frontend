f70c351a163d63b4273654a2c0834d7b917e1762
DESKTOP-PU1IG4U\Hyejin kim <beckey7124@naver.com>
2025-11-19 19:50:53 +0900

    메인 수정

--- js/main3.js
+++ js/main3.js
@@ -9,17 +9,17 @@ function changeRole(role) {
         if (role === "attack") {
             hero.src = "assets/img/hero/attack.png";
             title.textContent = "딜러";
-            desc.textContent = "빠른 공격으로 적을 제압하는 역할입니다.";
+            desc.textContent = "재빠른 기동력으로 전황을 바꾸고 팀에 기회를 여는 역할입니다.";
         }
         if (role === "tank") {
             hero.src = "assets/img/hero/Tanker.png";  // ⭐ 대문자 T 중요
             title.textContent = "탱커";
-            desc.textContent = "아군을 보호하는 역할입니다.";
+            desc.textContent = "팀을 향한 위협에 맞서며 전장을 통제하는 수호 기사입니다.";
         }
         if (role === "healer") {
             hero.src = "assets/img/hero/healer.png";
             title.textContent = "서포터";
-            desc.textContent = "아군을 회복시키는 지원형 역할입니다.";
+            desc.textContent = "팀을 안전하게 이끌어가는 전략적 서포트 역할입니다.";
         }
 
         hero.style.opacity = "1";
